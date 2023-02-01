c65b2232b7f9b687210ee9949557616ebb35a53d
Belzebulle <75722687+Belzebulle@users.noreply.github.com>
2023-02-02 00:28:42 +0100

    -

--- code/item_magic_candys.lua
+++ code/item_magic_candys.lua
@@ -13,10 +13,8 @@ function superCandy(player)
 end
 
 function mod:CandyStatUp(player, cacheFlags)
-
-    if superCandy(player, cacheFlags) then
-        player.MoveSpeed = player.MoveSpeed + candySpeed
-    end
+    
+    -- add speed if player has both items and only one time
     
     if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
 
@@ -32,6 +30,19 @@ function mod:CandyStatUp(player, cacheFlags)
         player.ShotSpeed = player.ShotSpeed + tearSpeedToAdd
 
         end
+
+
+    -- Not working
+    
+    --if player:GetData().candySpeed == true then
+    --    return
+    --end
+    --if superCandy(player) then
+    --        player.MoveSpeed = player.MoveSpeed + candySpeed
+    --        player:GetData().candySpeed = true
+    --end
+
+    
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.CandyStatUp)
@@ -77,7 +88,10 @@ function mod:useCandy(item, rng,  player)
     local tearSpeedToAdd = candyTearSpeed * itemCount
     player.Damage = player.Damage + damageToAdd
     player.ShotSpeed = player.ShotSpeed + tearSpeedToAdd
-    player:GetData().candy = true
+    player:GetData().candy = {
+        damage = damageToAdd,
+        tearSpeed = tearSpeedToAdd
+    }
 
     return {
         Discharge = true,
@@ -92,19 +106,16 @@ function mod:removeCandy()
 
     local player = Isaac.GetPlayer(0)
 
-    if player:GetData().candy == false then
-        return
+    if player:GetData().candy == nil or false then
+        return 
     end
 
-    local itemCount = player:GetCollectibleNum(magicCandy_1) + player:GetCollectibleNum(magicCandy_2)
-    if itemCount == 0 then
-        itemCount = 0.5
-    end
-    local damageToAdd = candyDamage * itemCount
-    local tearSpeedToAdd = candyTearSpeed * itemCount
-    player.Damage = player.Damage - damageToAdd
-    player.ShotSpeed = player.ShotSpeed - tearSpeedToAdd
-    player:GetData().candy = false
+    player.Damage = player.Damage - player:GetData().candy.damage
+    player.ShotSpeed = player.ShotSpeed - player:GetData().candy.tearSpeed
+    player:GetData().candy = {
+        damage = 0,
+        tearSpeed = 0
+    }
 end
 
 -- leave the room to remove the effect
