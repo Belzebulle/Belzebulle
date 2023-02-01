4f288deba7f0f5702c34d8fe4e2bc35815e8970f
Belzebulle <75722687+Belzebulle@users.noreply.github.com>
2023-02-01 23:31:09 +0100

    Add : new texture

--- main.lua
+++ main.lua
@@ -3,10 +3,11 @@ local mod = BELZEMOD
 
 
 -- include code files
-local item_magic_candy = require("code.item_magic_candy")
+local item_magic_candy = require("code.item_magic_candys")
 
 -- add description to items
 if EID then
     EID:addCollectible(Isaac.GetItemIdByName("Magic Bear"), "{{ArrowUp}} 1 damage", "Magic Bear")
     EID:addCollectible(Isaac.GetItemIdByName("Magic Lollipop"), "{{ArrowUp}} 0.25 tear speed", "Magic Lollipop")
+    EID:addCollectible(Isaac.GetItemIdByName("Magic Candy"), "{{ArrowUp}} 1 damage, {{ArrowUp}} 0.25 tear speed", "Magic Candy")
 end
