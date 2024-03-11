3e87d5668663eeac52cabaeb6e4a328efa9b2ab1
Belzebulle <hugo72vil@gmail.com>
2024-03-11 19:57:12 +0100

    --

--- code/gb.lua
+++ code/gb.lua
@@ -4,20 +4,19 @@ function mod:EvaluateItems(player, cacheFlags)
     local itemCount = player:GetCollectibleNum(Isaac.GetItemIdByName("Game Breaker?"))
 
     if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
-        -- ((𝜋/3)^(−y)⋅15−5 ) * z where y =player.Damage and z = item count
+        -- ((𝜋/3)^(−y)⋅15−5 ) * z where y = player.Damage and z = item count
         local damage = ((math.pi/3)^(-player.Damage)*15-5) * itemCount
-        print(damage)
         if damage <= 0.5 then damage = 0.5 * itemCount end
         player.Damage = player.Damage + damage
     end
 
     if cacheFlags & CacheFlag.CACHE_TEARFLAG == CacheFlag.CACHE_TEARFLAG then
         if itemCount > 0 then
-            player.TearFlags = player.TearFlags | TearFlags.TEAR_PIERCING
+            player.TearFlags = player.TearFlags | TearFlags.TEAR_SPECTRAL
         end
     end
 end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateItems, CacheFlag.CACHE_DAMAGE, CacheFlag.CACHE_TEARFLAG)
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateItems)
 
 -- Spawn Game Breaker? on first floor on the center of the room
 function mod:spawnItem()
@@ -30,5 +29,4 @@ function mod:spawnItem()
         Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, Isaac.GetItemIdByName("Game Breaker?"), pos, pos, player)
     end
 end
-
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.spawnItem)
